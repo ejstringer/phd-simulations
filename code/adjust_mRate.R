1830bdf5fd2d5f6aa784542654b9308cf8056ab3
ej stringer <ejstringer@live.com.au>
2024-03-25 16:11:37 +1100

    m adjust defined

---
+++ code/adjust_mRate.R
@@ -0,0 +1,19 @@
+
+#conditions adjust
+
+mAdjust <- c(I1 = 0,D1 = -0.02,L2 = 0.02,I2 =  -0.02,D2 = -0.02,L3 =  0, I3 = 0.02,D3 = -0.02)
+
+mAdjust <- c(I1 = 0,D1 = -0.04,L2 = 0.04,I2 =  -0.04,D2 = -0.04,L3 =  0, I3 = 0.04,D3 = -0.04)
+
+m_rate <- c(0.15, 0.09, 0.42, 0.66,0.3, 0.42, 0.66, 0.09)
+x <- m_rate
+
+
+for (i in 1:10) {
+  x<- x+mAdjust
+  
+  x <- ifelse(x <0 , 0, x)
+  x <- ifelse(x >= 1, 1, x)
+  print(unname(round(x,3)))
+}
+
