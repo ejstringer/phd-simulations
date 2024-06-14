3a8dbccaacf52d6f52e21e74eb4a5afe2e627092
Emily Stringer <ejstringer@live.com.au>
2024-06-14 10:32:21 +1000

    library located

--- code/libraries.R
+++ code/libraries.R
@@ -1,5 +1,4 @@
 # new 
-library(tidyverse, lib.loc = .libPaths()[2])
 library(tidyverse, lib.loc = 'C:/Program Files/R/R-4.1.2/library/')
 library(lubridate, lib.loc = 'C:/Program Files/R/R-4.1.2/library/')
 
@@ -19,29 +18,29 @@ library(officer, lib.loc = 'C:/Program Files/R/R-4.1.2/library/')
 
 
 # load libraries ---------------------------------------------------------------
-library(tidyverse, lib.loc = .libPaths()[2])
-library(lubridate, lib.loc = .libPaths()[2])
-library(dartR, lib.loc = .libPaths()[2])
-
-library(lme4)
-library(lmerTest)
-library(emmeans)
-
-library(scales)
-library(flextable)
-library(officer)
-
-library(gridExtra)
-library(grid)
-
-library(png)
-library(jpeg)
-
-# library(sf)
-# library(raster)
-# library(ggspatial)
-
-library(otuSummary)
-library(pbapply)
-
-library(zoo)
+# library(tidyverse, lib.loc = .libPaths()[2])
+# library(lubridate, lib.loc = .libPaths()[2])
+# library(dartR, lib.loc = .libPaths()[2])
+# 
+# library(lme4)
+# library(lmerTest)
+# library(emmeans)
+# 
+# library(scales)
+# library(flextable)
+# library(officer)
+# 
+# library(gridExtra)
+# library(grid)
+# 
+# library(png)
+# library(jpeg)
+# 
+# # library(sf)
+# # library(raster)
+# # library(ggspatial)
+# 
+# library(otuSummary)
+# library(pbapply)
+# 
+# library(zoo)
